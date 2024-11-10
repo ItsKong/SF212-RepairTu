7ecf45e9e7397930aa5cc8d521cd40fd98fe39a0
Kong <90138804+ItsKong@users.noreply.github.com>
2024-11-10 14:45:39 +0700

    Hotfix server.js

--- RepairTU-Backend/src/server.js
+++ RepairTU-Backend/src/server.js
@@ -14,14 +14,6 @@ app.use(express.json());
 //connect database
 connectAppDB();
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, '../../RepairTU-Frontend/dist')));
-
-// Catch-all route to serve index.html for any other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../RepairTU-Frontend/dist', 'index.html'));
-});
-
 app.get('/Hello', (req, res) => {
     res.send('Hello');
 })
@@ -48,6 +40,14 @@ app.get('/users/:id', async (req, res) => {
 
 app.use("/api", route);
 // app.use('/repairtuImage', express.static(path.join(__dirname, '../repairtuImage')));
+
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, '../../RepairTU-Frontend/dist')));
+
+// Catch-all route to serve index.html for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../RepairTU-Frontend/dist', 'index.html'));
+});
 
 app.listen(process.env.SERVER_PORT || 3000, () => {
     console.log("Server connected...");
